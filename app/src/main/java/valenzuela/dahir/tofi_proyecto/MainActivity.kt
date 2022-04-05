c24d5a9e0d0be30ef7f1f3ebee1c48184b6f5842
DahirVV <dahir.valenzuela@potros.itson.edu.mx>
2022-04-04 21:13:24 -0700

    BottomNav
    cree un bottom navigation para el menu principal

--- app/src/main/java/valenzuela/dahir/tofi_proyecto/MainActivity.kt
+++ app/src/main/java/valenzuela/dahir/tofi_proyecto/MainActivity.kt
@@ -2,10 +2,27 @@ package valenzuela.dahir.tofi_proyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import valenzuela.dahir.tofi_proyecto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.BottomNav
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.FragmentContainer) as NavHostFragment
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.inicioFragment, R.id.buscarFragment, R.id.bibliotecaFragment, R.id.actividadesFragment, R.id.perfilFragment)
+        )
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
