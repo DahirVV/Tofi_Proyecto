9f38f041e194feafe8aa818e7ab187ce7c4aa863
DahirVV <dahir.valenzuela@potros.itson.edu.mx>
2022-04-06 23:01:18 -0700

    pagina de perfiles

--- app/src/main/java/valenzuela/dahir/tofi_proyecto/Perfiles.kt
+++ app/src/main/java/valenzuela/dahir/tofi_proyecto/Perfiles.kt
@@ -1,11 +1,22 @@
 package valenzuela.dahir.tofi_proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Perfiles : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfiles)
+
+        val btn_img1: ImageButton = findViewById(R.id.cuenta1)
+        val btn_img2: ImageButton = findViewById(R.id.cuenta2)
+        val btn_img3: ImageButton = findViewById(R.id.cuenta3)
+
+        btn_img1.setOnClickListener{
+            var intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/valenzuela/dahir/tofi_proyecto/inicioLogin.kt
+++ app/src/main/java/valenzuela/dahir/tofi_proyecto/inicioLogin.kt
@@ -1,11 +1,19 @@
 package valenzuela.dahir.tofi_proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class inicioLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_login)
+        val button:Button = findViewById(R.id.btniniciarsesion)
+
+        button.setOnClickListener{
+            var intent: Intent = Intent(this, Perfiles::class.java)
+            startActivity(intent)
+        }
     }
 }
