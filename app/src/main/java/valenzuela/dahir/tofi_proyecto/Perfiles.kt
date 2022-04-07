df85ebd05d2efd473a6797e8d54ebb28291abcc0
DahirVV <dahir.valenzuela@potros.itson.edu.mx>
2022-04-06 18:37:06 -0700

    Agrege tipos de letras

---
+++ app/src/main/java/valenzuela/dahir/tofi_proyecto/Perfiles.kt
@@ -0,0 +1,11 @@
+package valenzuela.dahir.tofi_proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Perfiles : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfiles)
+    }
+}
