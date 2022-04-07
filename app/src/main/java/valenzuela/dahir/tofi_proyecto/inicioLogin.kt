568c311c7cdce8a5f1e110a87736e1294047a34e
DahirVV <dahir.valenzuela@potros.itson.edu.mx>
2022-04-06 18:05:56 -0700

    pantalla de inicio
    creeacion de pantalla de inicio

---
+++ app/src/main/java/valenzuela/dahir/tofi_proyecto/inicioLogin.kt
@@ -0,0 +1,11 @@
+package valenzuela.dahir.tofi_proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inicioLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_login)
+    }
+}
